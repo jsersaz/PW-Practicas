8f21fd2ea2eb5c7d59a0d81eec5757b9becb71d5
jsersaz <150834088+jsersaz@users.noreply.github.com>
2024-10-16 20:42:41 +0200

    Delete AbstractReservaFactory.java

--- AbstractReservaFactory.java
+++
@@ -1,8 +0,0 @@
-package es.uco.pw.ejercicio2;
-
-import java.time.LocalDate;
-
-public abstract class AbstractReservaFactory {
-	public abstract Reserva crearReservaIndividual(String tipo, String idReserva, LocalDate fecha, int duracion, int idPista, float precio, float descuento, int... participantes);
-	public abstract Reserva crearReservaBono(String tipo, String idReserva, LocalDate fecha, int duracion, int idPista, float precio, float descuento, String idBono, int numeroSesion, int... participantes);
-}
