429fe934ca462e5c23a78f7a070968fab60f2b78
i12sagud <117934929+i12sagud@users.noreply.github.com>
2024-11-12 11:42:23 +0100

    Delete es.uco.pw directory

--- es.uco.pw/src/es/uco/pw/business/material/Estado.java
+++
@@ -1,8 +0,0 @@
-package es.uco.pw.business.material;
-
-public enum Estado {
-	disponible,
-	reservado,
-	mal_estado,
-	none
-}
--- es.uco.pw/src/es/uco/pw/business/material/Tipo.java
+++
@@ -1,8 +0,0 @@
-package es.uco.pw.business.material;
-
-public enum Tipo {
-	pelotas, 
-	canastas,
-	conos,
-	none
-}
